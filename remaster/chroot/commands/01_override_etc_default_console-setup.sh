ddb2b39d716b62a12217cbdb030d0e049c613b1a
Sundar Nagarajan <sun.nagarajan@gmail.com>
2020-04-26 19:45:36 -0700

    Modify grub font and /etc/default/console-setup

---
+++ remaster/chroot/commands/01_override_etc_default_console-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install files related to making sound work
+
+PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
+PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
+PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
+
+SOURCE_FILE=${PROG_DIR}/../console-setup/console-setup
+DEST_FILE=/etc/default/console-setup
+
+if [ ! -f "$SOURCE_FILE" ]; then
+    echo "Source file not found: $SOURCE_FILE"
+    exit 1
+fi
+
+\cp -f "$SOURCE_FILE" "$DEST_FILE"
+exit $?
---
+++ remaster/iso_post/commands/01_set_grub_font.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# grub prefix - used with grub-mkimage
+GRUB_PREFIX="/boot/grub"
+
+PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
+PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
+PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
+ISO_EXTRACT_DIR=${PROG_DIR}/../..
+ISO_EXTRACT_DIR=$(readlink -e $ISO_EXTRACT_DIR)
+
+
+SCRIPT_DIR=${PROG_DIR}
+
+# Name of font file in GRUB_DIR
+FONT_FILE=font.pf2
+
+FONT_DEST_FILE="/${ISO_EXTRACT_DIR}/boot/grub${FONT_FILE}"
+FONT_SOURCE_FILE="${R_DIR}/iso_post/grub/${FONT_FILE}"
+if [ ! -f "${FONT_SOURCE_FILE}" ]; then
+    echo "Font file not found: ${FONT_SOURCE_FILE}"
+    exit 1
+fi
+
+\cp -f "$FONT_SOURCE_FILE" "$FONT_DEST_FILE"
+return $?
